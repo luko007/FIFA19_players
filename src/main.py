63ed5c4b93f292ed713785b66d516fb10323a8ec
Michael <michael.shechter@mail.huji.ac.il>
2019-06-08 01:10:06 +0300

    leaving ERROR: cannot convert string to float

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
 import pandas as pd
-import glob
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn import model_selection
-import os
-from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import svm
 import re
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 DATA_NAME = 'data.csv'
+FEET_TO_CM = re.compile (r"([0-9]+)'([0-9]*\.?[0-9]+)")
+
+
+
+def model(X, y):
+    X = np.nan_to_num(X)
+    y = np.nan_to_num(y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 19, shuffle=True)
+    clf = svm.SVC (kernel='linear', C=1, random_state=19).fit(X_train, y_train)
+    print(clf.score(X_test, y_test))
 
 def load_data():
     # Load all csv's to one file
@@ -24,18 +30,22 @@ def salary_to_number(data):
     if type(data) is str:
         return data.replace('M', '000000').replace('.', '').replace('€', '').replace('K', '000')
 
-feet_to_cm = re.compile (r"([0-9]+)'([0-9]*\.?[0-9]+)")
 def change_feet_to_cm(data):
     if type(data) is str:
-        m = feet_to_cm.match (data)
+        m = FEET_TO_CM.match (data)
         if m == None:
             return float ('NaN')
         else:
             return int (m.group(1)) * 12 + float (m.group (2))
 
 def pre_proccess(data):
+    y = data['Potential']
+    data.drop(['Potential'], axis=1, inplace=True)
+
     # Deal with nan ==> 0
-    data = data.fillna(0)
+    data.fillna(0, inplace=True)
+    data.replace('', 0, inplace=True)
+
 
     columns_to_delete = ['ID', 'Photo', 'Flag', 'Club Logo', 'Body Type', 'Real Face', 'Name']
     [data.drop([col_to_del], axis=1, inplace=True) for col_to_del in columns_to_delete]
@@ -82,15 +92,15 @@ def pre_proccess(data):
     natio_dummy = pd.get_dummies(data['Work Rate'])
     data = pd.concat([data, natio_dummy], axis=1)
     data.drop(['Work Rate'], axis=1, inplace=True)
-
-
-
-    print(data.head())
+    data.replace('', 0.0, inplace=True)
+    data = np.nan_to_num(data)
+    return data, y
 
 
 def main():
     data = load_data()
-    pre_proccess(data)
+    X, y = pre_proccess(data)
+    model(X, y)
 
 
 if __name__ == "__main__":
