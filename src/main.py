10b1041fa741ff4c26f032d7eaa9bc733c23486d
Michael <michael.shechter@mail.huji.ac.il>
2019-06-08 22:04:13 +0300

    Found optimal depth and number of trees to Random Forest using Grid Search (15, 500)

--- src/main.py
+++ src/main.py
@@ -8,10 +8,11 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, max_error, median_absolute_error
-
+from sklearn.linear_model import LassoCV
+from sklearn.ensemble import AdaBoostRegressor
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import GridSearchCV
-
+import time
 from sklearn import svm
 import re
 import numpy as np
@@ -19,7 +20,6 @@ from sklearn.model_selection import train_test_split
 
 DATA_NAME = 'data.csv'
 FEET_TO_CM = re.compile (r"([0-9]+)'([0-9]*\.?[0-9]+)")
-
 
 
 def model(X, y):
@@ -40,49 +40,74 @@ def model(X, y):
     # b = [b for b in a]
     # print([isnumeric(s) for s in b])
 
-    # print("SVM with SGD")
-    # fit_and_predict(X_train, y_train, X_test, y_test,
-    #                 SGDClassifier (loss='hinge', random_state=41, n_jobs=-1,
-    #                max_iter=1000, tol=None))
-    # print("Linear SVM")
-    # lin_svc = LinearSVC(tol=1e-5, random_state=19).fit(X_train, y_train)
-    # y_pred = lin_svc.predict(X_test)
-    # regressor_scoring (y_pred, y_test)
+    # svm_learner (X_test, X_train, y_test, y_train)
+
+    # linear_reg (X_test, X_train, y_test, y_train)
+
+    random_forest (X_test, X_train, y_test, y_train)
+
+    # lasso (X_test, X_train, y_test, y_train)
+
+    # knn(X_test, X_train, y_test, y_train)
 
 
-    print("Linear Regression")
+def svm_learner(X_test, X_train, y_test, y_train):
+    print ("SVM with SGD")
+    fit_and_predict (X_train, y_train, X_test, y_test,
+                     SGDClassifier (loss='hinge', random_state=41, n_jobs=-1,
+                                    max_iter=1000, tol=None))
+    print ("Linear SVM")
+    lin_svc = LinearSVC (tol=1e-5, random_state=19).fit (X_train, y_train)
+    y_pred = lin_svc.predict (X_test)
+    regressor_scoring (y_pred, y_test)
+
+
+def linear_reg(X_test, X_train, y_test, y_train):
+    print ("Linear Regression")
     # normalize=True ?
-    lin_reg = LinearRegression().fit(X_train, y_train)
-    y_pred = lin_reg.predict(X_test)
+    lin_reg = LinearRegression ().fit (X_train, y_train)
+    y_pred = lin_reg.predict (X_test)
     regressor_scoring (y_pred, y_test, "Linear Regression")
 
 
-    print("\nRandom Forest Regressor")
+def lasso(X_test, X_train, y_test, y_train):
+    print ("\nLasso")
+    ridge = LassoCV (eps=1, alphas=[1e-2, 1e-3, 1e-4, 1e-5], tol=0.1, cv=5).fit (X_train, y_train)
+    y_pred = ridge.predict (X_test)
+    regressor_scoring (y_pred, y_test, "Lasso")
 
+
+def random_forest(X_test, X_train, y_test, y_train):
+    print ("\nRandom Forest Regressor")
+    OPTIMAL_DEPTH = 15
+    OPTIMAL_EST = 500
+    regr = RandomForestRegressor (max_depth=OPTIMAL_DEPTH,
+                                  n_estimators=OPTIMAL_EST,
+                                  random_state=18, )
+    regr = AdaBoostRegressor (regr, n_estimators=300, random_state=19)
+    regr.fit (X_train, y_train)
+    y_pred = regr.predict (X_test)
+    regressor_scoring (y_pred, y_test, "Random Forest")
+
+
+def grid_search_random_reg(X_train, y_train):
+    """Results were:
+    {'max_depth': 15, 'n_estimators': 500}
+    Random Forest MSE is: 1.39288676662
+    Random Forest Max error is: 10.322
+    Random Forest Median error of: 0.438544869642
+    """
     gs_random_reg = GridSearchCV (
         estimator=RandomForestRegressor (),
         param_grid={
-            'max_depth': [3,9,15,25],
+            'max_depth': [3, 9, 15, 25],
             'n_estimators': (150, 100, 500, 1000),
         },
         cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
-
-    grid_result = gs_random_reg.fit(X_train, y_train)
+    grid_result = gs_random_reg.fit (X_train, y_train)
     best__random_reg_params = grid_result.best_params_
-    print(best__random_reg_params)
-    regr = RandomForestRegressor(max_depth=best__random_reg_params['max_depth'],
-                                 n_estimators=best__random_reg_params['n_estimators'],
-                                 random_state=18,).fit(X_train, y_train)
-    y_pred = regr.predict(X_test)
-    regressor_scoring(y_pred, y_test, "Random Forest")
-
-    from sklearn.linear_model import Lasso, LassoCV
-    print("\nLasso")
-    ridge = LassoCV(eps=1, alphas=[1e-2,1e-3,1e-4,1e-5], tol=0.1, cv=5).fit(X_train, y_train)
-    y_pred = ridge.predict(X_test)
-    regressor_scoring (y_pred, y_test, "Lasso")
-
-    # knn(X_test, X_train, y_test, y_train)
+    print (best__random_reg_params)
+    return best__random_reg_params
 
 
 def regressor_scoring(y_pred, y_test, name):
@@ -204,4 +229,6 @@ def main():
 
 
 if __name__ == "__main__":
+    start = time.clock()
     main()
+    print("Took %s seconds" % time.clock() - start)
